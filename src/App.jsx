630d4aad2815d2dad92a282a981d445df3564489
Ligia Gomes V Souza <166971095+Ligiagomess@users.noreply.github.com>
2024-06-07 22:05:29 +0000

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   }
 
   const item3 = {
-    name: 'Summer Smith hguihjh',
+    name: 'Summer Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
 
