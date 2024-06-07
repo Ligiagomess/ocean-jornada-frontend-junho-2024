aecc05c589b06f4fd1387e95c252a86dbc4a5a01
Ligia Gomes V Souza <166971095+Ligiagomess@users.noreply.github.com>
2024-06-07 13:17:30 +0000

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ function App() {
   return (
     <>
       <div className="card">
-        <h2>Rick Sanchez</h2>
-        
-        <div className='tags'>
-          <div>Status: Vivo</div>
-          <div>Espécie: Humana</div>
-          <div>Origem: Terra</div>
+            <h2>Rick Sanchez</h2>
+
+            <div className='tags'>
+            <div>Status: Vivo</div>
+            <div>Espécie: Humana</div>
+            <div>Origem: Terra</div>
         </div>
 
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-      </div>
+     </div>
     </>
   )
 }
